14b93725061a5fbb9e3d9773b6c58d05878d38e4
Julian Alconcher <alconcherjulian@gmail.com>
2024-05-19 19:12:08 -0300

    Add consumo de API externa

--- app/ui/MovieContent.tsx
+++ app/ui/MovieContent.tsx
@@ -1,36 +1,45 @@
-import React from 'react'
-import Image from 'next/image'
+import React from 'react';
+import Image from 'next/image';
 
 interface MovieCardProps {
     movie: string;
     image: string;
     description: string;
     videoTrailer: string;
-  }
-  
+    releaseDate: string;
+}
 
-  const MovieContent: React.FC<MovieCardProps> = ({ movie, description, image, videoTrailer }) => {
+const MovieContent: React.FC<MovieCardProps> = ({ movie, description, image, videoTrailer, releaseDate }) => {
+
+    const embedUrl = videoTrailer.includes('embed') ? 
+    videoTrailer : `https://www.youtube.com/embed/${videoTrailer.split('v=')[1]}`;
+
     return (
-      <div className='md:flex'>
-          <iframe 
-              className='hidden md:flex md:px-4'
-              width="900" 
-              height="620" 
-              src={videoTrailer} 
-              title="YouTube video player" 
-          >
-          </iframe>
-          <div className='p-2 flex-1 flex flex-col items-center '>
-              <h1 className='md:text-6xl text-4xl text-center'>{movie}</h1>
-              <div className='md:flex'>
-                <div className=''>
-                    <Image src={image} width={400} height={600} alt={movie} className='p-6 md:w-96 ' />
+        <div className='md:flex'>
+            <iframe 
+                className='hidden md:flex'
+                width="900" 
+                height="620" 
+                src={embedUrl} 
+                title="YouTube video player" 
+                allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" 
+                allowFullScreen
+            ></iframe>
+            <div className=' flex-1 flex flex-col items-center md:mt-3'>
+                <h1 className='md:text-6xl text-4xl text-center'>{movie}</h1>
+                <div className='md:flex md:mt-3'>
+                    <div>
+                        <Image src={image} width={400} height={600} alt={movie} className='md:w-80 mt-2' />
+                    </div>
+                    <div className='md:flex-1 text-center md:p-2 md:pl-4'>
+                        <span className='text-4xl'>Descripcion: </span>
+                        <p className='md:text-2xl text-l flex-1 text-center'>{description}</p>
+                        <h3 className='md:text-2xl text-center md:text-center md:mt-2'>Disponible a partir del dia: {releaseDate}</h3>
+                    </div>
                 </div>
-                <p className='md:text-2xl text-l flex-1 text-center md:text-left md:mt-2'>{description}</p>
-              </div>
-          </div>
-      </div>
-    )
-  }
+            </div>
+        </div>
+    );
+}
 
-export default MovieContent
+export default MovieContent;
--- app/ui/MoviesCarousel.tsx
+++ app/ui/MoviesCarousel.tsx
@@ -1,41 +1,74 @@
+"use client";
 import { Carousel, CarouselContent, CarouselItem, CarouselNext, CarouselPrevious } from '@/components/ui/carousel'
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import MovieContent from './MovieContent'
+import fetch from 'node-fetch';
 
-const products = [
-    {
-      movie: 'La Era del Hielo 3',
-      image: '/img/laEraDelHielo.jpg',
-      description: 'La Era de Hielo es una película de animación clave. Esta pelicula fue desarrollada por el estudio de animacion de DreamWorks Animation. ',
-      videoTrailer: 'https://www.youtube.com/watch?v=wHhCZCdKZk8&ab_channel=20thCenturyStudiosHomeEntertainmentEspa%C3%B1a'
-    },
-    {
-        movie: 'Madagascar 3',
-        image: '/img/madagascar.jpg',
-        description: 'Madagascar 3 es una película de animación de 2012 estrenada por Pixar Animation Studios. Es la tercera película animada de Walt Disney Animation Studios. ',
-        videoTrailer: 'https://www.youtube.com/watch?v=QqVb9OoQh7g'
-      },
-  ];
+
+const url = 'https://api.themoviedb.org/3/movie/now_playing?language=en-US&page=1';
+const options = {
+  method: 'GET',
+  headers: {
+    accept: 'application/json',
+    Authorization: 'Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiJjYTM4NzIyNDJkZTk5ZTE3YWI4YTBhY2UyMTk5OGYwMiIsInN1YiI6IjY2MzY5YWIzMDMyOGI5MDEyZGRiZTUxYyIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.qjr273qiq2tf3JvCIl2FqXMNGpWdiKj-74q8e4VWB_I'
+  }
+};
+
+interface Movie {
+  title: string;
+  poster_path: string;
+  overview: string;
+  videoTrailer: string;
+  release_date: string;
+}
+
+interface MovieData {
+  movie: string;
+  image: string;
+  description: string;
+  videoTrailer: string;
+  releaseDate: string;
+}
 
 export const MoviesCarousel = () => {
+  const [movies, setMovies] = useState<MovieData[]>([]);
+
+  useEffect(() => {
+    fetch(url, options)
+      .then((res) => res.json())
+      .then((json) => {
+        console.log(json);
+        const firstFiveMovies = json.results.slice(0, 5).map((movie: Movie) => ({
+          movie: movie.title,
+          image: `https://image.tmdb.org/t/p/w500${movie.poster_path}`,
+          description: movie.overview,
+          videoTrailer: `https://www.youtube.com/results?search_query=${movie.title} trailer`,
+          releaseDate: movie.release_date
+        }));
+        setMovies(firstFiveMovies);
+        console.log(firstFiveMovies);
+      })
+      .catch((err) => console.error('error:' + err));
+  }, []);
+
   return (
     <div className='bg-black text-white'>
-        <Carousel className="w-full " >
+      <Carousel className="w-full">
         <h2 className="text-center text-5xl mb-4 py-2 tracking-tighter sm:text-4xl md:text-5xl">Películas en Cine</h2>
-        <CarouselContent className=" mb-2">
-        {products.map((product, index) => (
-            <CarouselItem key={index} className=" md:basis-1/1 lg:basis-1/1">
-            <div className="p-2">
-                <MovieContent movie={product.movie} image={product.image} description={product.description} videoTrailer={product.videoTrailer}/>
-            </div>
+        <CarouselContent className="mb-2">
+          {movies.map((movie, index) => (
+            <CarouselItem key={index} className="md:basis-1/1 lg:basis-1/1">
+              <div className="p-2">
+                <MovieContent movie={movie.movie} image={movie.image} description={movie.description} videoTrailer={movie.videoTrailer} releaseDate={movie.releaseDate}/>
+              </div>
             </CarouselItem>
-        ))}
+          ))}
         </CarouselContent>
         <CarouselPrevious />
         <CarouselNext />
-    </Carousel>
-  </div>
-  )
+      </Carousel>
+    </div>
+  );
 }
 
-export default MoviesCarousel
+export default MoviesCarousel;

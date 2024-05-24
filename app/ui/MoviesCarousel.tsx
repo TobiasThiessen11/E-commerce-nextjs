fc2f7499a2aab2e4c1ba9b26e0cd7493afe311f0
Julian Alconcher <alconcherjulian@gmail.com>
2024-05-24 15:35:15 -0300

    Extract fetch de API externa y colocado en data.ts

--- app/ui/MoviesCarousel.tsx
+++ app/ui/MoviesCarousel.tsx
@@ -1,9 +1,8 @@
 "use client";
-import { Carousel, CarouselContent, CarouselItem, CarouselNext, CarouselPrevious } from '@/components/ui/carousel'
-import React, { useEffect, useState } from 'react'
-import MovieContent from './MovieContent'
-import fetch from 'node-fetch';
-
+import { Carousel, CarouselContent, CarouselItem, CarouselNext, CarouselPrevious } from '@/components/ui/carousel';
+import React, { useEffect, useState } from 'react';
+import MovieContent from './MovieContent';
+import { fetchLatestAnimatedMovies } from '@/lib/data';
 
 const url = 'https://api.themoviedb.org/3/movie/now_playing?language=en-US&page=1';
 const options = {
@@ -14,15 +13,6 @@ const options = {
   }
 };
 
-interface Movie {
-  title: string;
-  poster_path: string;
-  overview: string;
-  videoTrailer: string;
-  release_date: string;
-  genre_ids: number[];
-}
-
 interface MovieData {
   movie: string;
   image: string;
@@ -31,27 +21,16 @@ interface MovieData {
   releaseDate: string;
 }
 
-export const MoviesCarousel = () => {
-  const [movies, setMovies] = useState<MovieData[]>([]);
+const MoviesCarousel: React.FC = () => {
+  const [movies, setMovies] = useState<MovieData[] | null>(null);
 
   useEffect(() => {
-    fetch(url, options)
-      .then((res) => res.json())
-      .then((json) => {
-        console.log(json);
-        if (json.results && Array.isArray(json.results)){}
-        const AnimatedMovies = json.results.filter((movie: Movie) => movie.genre_ids.includes(16));
+    const fetchData = async () => {
+      const data = await fetchLatestAnimatedMovies(url, options);
+      setMovies(data);
+    };
 
-        const firstFiveMovies = AnimatedMovies.slice(0, 5).map((movie: Movie) => ({
-          movie: movie.title,
-          image: `https://image.tmdb.org/t/p/w500${movie.poster_path}`,
-          description: movie.overview,
-          videoTrailer: `https://www.youtube.com/results?search_query=${movie.title} trailer`,
-          releaseDate: movie.release_date
-        }));
-        setMovies(firstFiveMovies);
-      })
-      .catch((err) => console.error('error:' + err));
+    fetchData();
   }, []);
 
   return (
@@ -59,7 +38,7 @@ export const MoviesCarousel = () => {
       <Carousel className="w-full">
         <h2 className="text-center text-5xl mb-4 py-2 tracking-tighter sm:text-4xl md:text-5xl">Películas Animadas en Cine</h2>
         <CarouselContent className="mb-2">
-          {movies.map((movie, index) => (
+          { movies?.map((movie, index) => (
             <CarouselItem key={index} className="md:basis-1/1 lg:basis-1/1">
               <div className="p-2">
                 <MovieContent movie={movie.movie} image={movie.image} description={movie.description} videoTrailer={movie.videoTrailer} releaseDate={movie.releaseDate}/>

7847420701f502f0cd4631dd02a9350d62f12e4f
Julian Alconcher <alconcherjulian@gmail.com>
2024-05-27 16:37:59 -0300

    Fix responsive

--- app/product/[p_id]/page.tsx
+++ app/product/[p_id]/page.tsx
@@ -17,53 +17,49 @@ export default async function Page({ params }: { params: { p_id: string } }) {
     }));
     
     return(
-        <div className="h-screen mb-8">
-            <div className="font-sans p-6 max-w-5xl mx-auto bg-gray-100 h-screen rounded-lg shadow-md ">
-            <div className="grid md:grid-cols-2 gap-6 lg:gap-12 items-start max-w-6xl px-4 mx-auto py-12">
-             <div className="grid gap-4 md:gap-10 items-start">
-             <MyGallery images={images}/>
-            </div>
-            <div className="grid gap-4 md:gap-10 items-start">
-                <div className=" md:flex items-start">
-                <div className="grid gap-4 ">
-                        <h1 className="font-bold text-3xl lg:text-4xl">{product.name}</h1>
-                    <div>
-                        <p>{product.description}</p>                    
+        <div className="m-5">
+            <div className="font-sans max-w-5xl mx-auto bg-gray-100 rounded-lg shadow-md ">
+            <div className="grid md:grid-cols-2 gap-6 lg:gap-12 items-start max-w-6xl px-4 mx-auto py-10">
+                <div className="grid gap-4 md:gap-10 items-start">
+                <MyGallery images={images}/>
+                </div>
+                <div className="grid gap-4 md:gap-10 items-start">
+                    <div className=" md:flex items-start">
+                    <div className="grid gap-4 ">
+                            <h1 className="font-bold text-3xl lg:text-4xl">{product.name}</h1>
+                        <div>
+                            <p>{product.description}</p>                    
+                        </div>
                     </div>
-                </div>
-                    
-                </div>
-                <div className="text-4xl font-bold">$ {product.price}</div>
-                <form className="grid gap-4 ">
-                <div className="grid gap-2">
-                    <span className="text-gray-500">Cantidad</span>
-                    
-                    <Select defaultValue="1">
-                    <SelectTrigger className="w-24">
-                        <SelectValue placeholder="Select" />
-                    </SelectTrigger>
-                    <SelectContent>
-                        <SelectItem value="1">1</SelectItem>
-                        <SelectItem value="2">2</SelectItem>
-                        <SelectItem value="3">3</SelectItem>
-                        <SelectItem value="4">4</SelectItem>
-                        <SelectItem value="5">5</SelectItem>
-                    </SelectContent>
-                    </Select>
-                </div>
-                    <Button className="w-full" size="lg">
-                        Añadir al Carrito
-                    </Button>
-                    <Button className="w-full" size="sm">
-                        COMPRAR
-                    </Button>
-                </form>
-                <div className="grid gap-4 md:gap-10 text-sm leading-loose">
-                <p>
-                    {product.description}
-                </p>
-                </div>
-            </div>
+                        
+                    </div>
+                    <div className="text-4xl font-bold">$ {product.price}</div>
+                    <form className="grid gap-4 ">
+                    <div className="grid">
+                        <span className="text-gray-500">Cantidad</span>
+                        
+                        <Select defaultValue="1">
+                        <SelectTrigger className="w-24">
+                            <SelectValue placeholder="Select" />
+                        </SelectTrigger>
+                        <SelectContent>
+                            <SelectItem value="1">1</SelectItem>
+                            <SelectItem value="2">2</SelectItem>
+                            <SelectItem value="3">3</SelectItem>
+                            <SelectItem value="4">4</SelectItem>
+                            <SelectItem value="5">5</SelectItem>
+                        </SelectContent>
+                        </Select>
+                    </div>
+                        <Button className="w-full" size="sm">
+                            Añadir al Carrito
+                        </Button>
+                        <Button className="w-full" size="sm">
+                            COMPRAR
+                        </Button>
+                    </form>
+                        
+                    </div>
             </div>
             </div>
         </div>

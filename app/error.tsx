7fb9c1c8c5d109bc30c05dc9854e5c14f0525008
TobiasThiessen11 <tobiasthiessen@live.com.ar>
2024-05-30 20:08:29 -0300

    Edit Estilo pagina error

--- app/error.tsx
+++ app/error.tsx
@@ -2,6 +2,7 @@
  
 import Link from 'next/link';
 import { useEffect } from 'react';
+import Image from 'next/image';
  
 export default function Error({
   error,
@@ -16,8 +17,16 @@ export default function Error({
   }, [error]);
  
   return (
-    <main className="flex h-screen flex-col items-center justify-center">
-      <h2 className="text-center text-3xl">Algo anduvo mal!</h2>
+    <main className="flex h-screen flex-col items-center justify-start pt-16">
+      <Image
+              src="/img/lotso-triste-error.png"
+              width={1000}
+              height={1000}
+              alt="Lotso sad error"
+              className="w-1/4 h-auto"
+            />
+      <h2 className="text-center text-3xl mt-4">¡UPS! ALGO SALIO MAL</h2>
+      <p className="gray-300 text-center text-xl">Parece que hay un problema técnico porque no podemos encontrar lo que buscas</p>
       <Link
         href="/"
         className="mt-4 rounded-md bg-blue-500 px-4 py-2 text-sm text-white transition-colors hover:bg-blue-400"
@@ -26,4 +35,5 @@ export default function Error({
       </Link>
     </main>
   );
+  
 }

7f6eb06abc12942220026abefe74874bf4480d29
Julian Alconcher <alconcherjulian@gmail.com>
2024-06-01 17:09:01 -0300

    Botones de filtro funcionando

--- app/products/page.tsx
+++ app/products/page.tsx
@@ -1,4 +1,4 @@
-import React, { Suspense } from 'react';
+import React from 'react';
 import Filter from './ui/Filter';
 import Card from '../ui/Card';
 import Banner from './ui/Banner';
@@ -18,14 +18,15 @@ export default async function Products({
   const query = searchParams?.query || '';
   const currentPage = Number(searchParams?.page) || 1;
   const totalPages = await fetchProductsPages(query);
+  
   const productsFilter = await fetchProductsByName(query, currentPage);
   if (!productsFilter) return notFound();
+  
   const images: ProductImage[] = await fetchProductsImages();
   if (!images) return notFound();
 
   return (
     <div>
-      <Banner image="/img/banner.png" title="Clasicos" description="Hasta un -40%" />
       <div className="md:flex h-full">
         <div className="">
           <Filter />
--- app/products/ui/Filter.tsx
+++ app/products/ui/Filter.tsx
@@ -1,44 +1,77 @@
-import { fetchCategories, fetchMovies } from '@/lib/dataDB'
+"use client";
+import { usePathname, useRouter, useSearchParams } from 'next/navigation';
 import React from 'react'
+import { useDebouncedCallback } from 'use-debounce';
 
 
-export default async function Filter() {
+export default function Filter() {
+    const searchParams = useSearchParams();
+    const pathname = usePathname();
+    const { replace } = useRouter();
+    
+    const categories = [
+        "Todos",
+        "Peluches",
+        "Juguetes",
+        "Coleccionables",
+        "Otro",
+    ];  
 
-  const categories = await fetchCategories();
-  const movies = await fetchMovies();
+    const movies = [
+        "Rapunzel",
+        "Star Wars",
+        "Lilo y Stitch",
+        "Toy Story",
+        "Wall-E",
+        "Tarzan",
+        "La era del Hielo"
+    ]; 
 
-  return (
-    <div className="md:flex flex-col md:flex-row gap-8  md:p-5  ">
-        <div className='bg-neutral rounded-lg shadow-xl p-6 md:w-64 space-y-6'>
-            <div className='flex flex-col text-center'>
-                <h1 className='text-3xl font-bold'>CATEGORIAS</h1>
+    const handleSearch = useDebouncedCallback((term) => {
+        const params = new URLSearchParams(searchParams);
+        params.set('page', '1');
+        if(term === "Todos") {
+            params.delete('query');
+        }
+        if (term) {
+          params.set('query', term);
+        } else {
+          params.delete('query');
+        }
+        replace(`${pathname}?${params.toString()}`);
+      }, 300);
+    return (
+        <div className="md:flex flex-col md:flex-row gap-8  md:p-5  ">
+            <div className='bg-neutral rounded-lg shadow-xl p-6 md:w-64 space-y-6'>
+                <div className='flex flex-col text-center'>
+                    <h1 className='text-3xl font-bold'>CATEGORIAS</h1>
+                    {categories.map((category) => (
+                        <div key={category}>
+                            <label>
+                                <button className='p-2 w-full selection:bg-black bg-white rounded-lg my-2' onClick={() => handleSearch(category)}>
+                                    {category}
+                                </button>
+                            </label>
+                        </div>
+                    ))}
+                </div>
 
-                {categories.map((category) => (
-                    <div key={category.c_id}>
-                    <label>
-                        <button className='p-2 w-full selection:bg-black bg-white rounded-lg my-2'>{category.name}</button>
-                    </label>
-                    </div>
-                ))}
-                
-            </div>
-
-            <div className='flex flex-col text-center'>
-                <h1 className='text-3xl font-bold'>PELÍCULAS</h1>
-
-                {movies.map((movie) => (
-                    <div key={movie.m_id}>
-                    <label>
-                        <button className='p-2 w-full selection:bg-black bg-white rounded-lg my-2'>{movie.name}</button>
-                    </label>
-                    </div>
-                ))}
-                
+                <div className='flex flex-col text-center'>
+                    <h1 className='text-3xl font-bold'>PELÍCULAS</h1>
+                    {movies.map((movie) => (
+                        <div key={movie}>
+                            <label>
+                                <button className='p-2 w-full selection:bg-black bg-white rounded-lg my-2' onClick={() => handleSearch(movie)}>
+                                    {movie}
+                                </button>
+                            </label>
+                        </div>
+                    ))}
+                </div>
             </div>
         </div>
-    </div>
-    
-  )
+        
+    )
 }
 
 

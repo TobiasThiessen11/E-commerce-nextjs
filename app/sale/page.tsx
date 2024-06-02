3481cce844a394a86ffd6f7bd6ca3fcb6e03b507
Julian Alconcher <alconcherjulian@gmail.com>
2024-06-02 18:50:31 -0300

    Fix deploy

--- app/sale/page.tsx
+++ app/sale/page.tsx
@@ -3,8 +3,6 @@ import Banner from "../products/ui/Banner"
 
 export default async function Page () {
 
-    const images = await getImages();
-    console.log(images);
     return (
         <>
         <Banner image="/img/banner.png" title="Oferta" description="Descuentos de la semana"/>

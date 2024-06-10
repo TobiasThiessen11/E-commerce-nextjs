48ddf6b2e6315e70bdd24d32eca996977ca07bf0
Julian Alconcher <alconcherjulian@gmail.com>
2024-06-10 15:15:09 -0300

    Add insert into sales table

--- app/api/create-preference/route.ts
+++ app/api/create-preference/route.ts
@@ -1,10 +1,7 @@
-
 import { MercadoPagoConfig, Preference } from 'mercadopago';
-import { NextApiRequest, NextApiResponse } from 'next';
 import { NextRequest, NextResponse } from 'next/server';
 
 const client = new MercadoPagoConfig({ accessToken: "TEST-5113519851838077-060615-2bbfd7b8a6495ed0b67f8f618bb0f4a3-272885138" });
-
 
 export async function POST(request: NextRequest) {
   try {
@@ -16,20 +13,19 @@ export async function POST(request: NextRequest) {
     const preference = new Preference(client);
 
     const response = await preference.create({
-        body: {
-          external_reference: '123',
-          items: items.map((item: { name: string; quantity: number; price: string }) => ({
-            title: item.name,
-            quantity: item.quantity,
-            unit_price: Number(item.price),
-          }))
-        }
-      })
-  
-      return NextResponse.json({ preferenceId: response.id });
-    } catch (error) {
-      console.error(error);
-      
-      return NextResponse.json({ message: 'Internal server error' }, { status: 500 });
-    }
-  };
+      body: {
+        external_reference: '123',
+        items: items.map((item: { name: string; quantity: number; price: number }) => ({
+          title: item.name,
+          quantity: item.quantity,
+          unit_price: Number(item.price) ,
+        }))
+      }
+    });
+
+    return NextResponse.json({ preferenceId: response.id, items });
+  } catch (error) {
+    console.error(error);
+    return NextResponse.json({ message: 'Internal server error' }, { status: 500 });
+  }
+}
--- app/payment/route.ts
+++ app/payment/route.ts
@@ -1,24 +1,48 @@
-import type {NextRequest} from 'next/server';
+import type { NextRequest } from 'next/server';
 import { MercadoPagoConfig, Payment } from 'mercadopago';
-import { Sale,SaleDetail } from '@/lib/definitions';
+import { sql } from '@vercel/postgres';
 
 const client = new MercadoPagoConfig({ accessToken: "TEST-5113519851838077-060615-2bbfd7b8a6495ed0b67f8f618bb0f4a3-272885138" });
 
 export async function POST(request: NextRequest) {
-    const body = await request.json().then((data) => data as {data: {id:string}});
-    const payment = await new Payment(client).get({id:body.data.id});
+  const body = await request.json().then((data) => data as { data: { id: string }, items: { id: string, name: string, price: number, quantity: number }[] });
+  const payment = await new Payment(client).get({ id: body.data.id });
+
+  const sale = {
+    transaction_mp_id: payment.id,
+    person_email: payment.payer?.email,
+  };
+
+  try {
+    // Iniciar la transacción
+    await sql`BEGIN`;
+
+    // Insertar en la tabla sales
+    const saleResult = await sql`
+        INSERT INTO sales (date,transaction_mp_id, person_email)
+        VALUES (CURRENT_DATE,${sale.transaction_mp_id}, ${sale.person_email})
+        RETURNING s_id;
+
+    `;
+
+    // Insertar detalles de los productos en la tabla salesDetails
+    // const items = body.items;
+    // console.log(items);
+    // for (const item of items) {
+    //     await sql`
+    //       INSERT INTO salesDetails (price, quantity, subtotal, sale_id, product_id)
+    //       VALUES (${item.price}, ${item.quantity}, ${item.price * item.quantity}, ${saleResult.rows[0].s_id}, ${item.id});
+    //     `;
+    // }
     
-    console.log("Nuestro pago:", payment);
-    const sale = {
-        transaction_mp_id: payment.id,
-        person_email: payment.payer?.email,
-    }
 
-    const saleDetail = {
-        amount: payment.transaction_amount,
-
-    }
-
-    console.log("Nuestra venta:", sale);
+    // Confirmar la transacción
+    await sql`COMMIT`;
     return new Response('OK', { status: 200 });
+  } catch (error) {
+    // Revertir la transacción en caso de error
+    await sql`ROLLBACK`;
+    console.error('Error inserting into database:', error);
+    return new Response('Internal Server Error', { status: 500 });
+  }
 }

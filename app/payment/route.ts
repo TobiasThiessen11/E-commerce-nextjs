dda8572fd28666341c1db0cac3a310c24d77583e
Julian Alconcher <alconcherjulian@gmail.com>
2024-06-26 09:19:25 -0300

    Fix mercadopago webhook

--- app/payment/route.ts
+++ app/payment/route.ts
@@ -1,24 +1,26 @@
-import type { NextRequest } from 'next/server';
+import { NextResponse, type NextRequest } from 'next/server';
 import { MercadoPagoConfig, Payment } from 'mercadopago';
 import { sql } from '@vercel/postgres';
 
 const client = new MercadoPagoConfig({
-  accessToken: process.env.MP_ACCESS_TOKEN!
+  accessToken: process.env.MP_ACCESS_TOKEN!,
 });
 
 export async function POST(request: NextRequest) {
-  const body = await request.json().then((data) => data as {data: {id:string}});
-  const payment = await new Payment(client).get({id:body.data.id});
-
-  console.log("payment:", payment);
-  console.log("external_reference:", payment.external_reference);
-
   try{
-    await sql`UPDATE sales SET transaction_mp_id=${payment.id} WHERE s_id=${payment.external_reference}`;
-  }catch(e){
-    console.error(e);
-    return new Response('Internal server error', { status: 500 });
-  }
+  const body = await request.json();
 
-  return new Response('OK', { status: 200 });
+  const payment = await new Payment(client).get({ id: body.data.id });
+
+  if (payment && payment.status === "approved") {
+    await sql`UPDATE sales SET transaction_mp_id=${payment.id} WHERE s_id=${payment.external_reference}`;
+    return NextResponse.json({ success: true });
+  }
+  else{
+    return NextResponse.json({ success: false });
+  }
+  }catch (error: any) {
+    console.error("Error processing payment:", error);
+    return Response.json({ success: false, error: error.message });
+  }
 }
